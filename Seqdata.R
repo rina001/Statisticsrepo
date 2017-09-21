ea30070f926f9cc4dc88212d5573aa0be8e6b006
rina001 <rinaqiu88@yahoo.com>
2017-09-20 21:37:41 -0700

    To commit this datasets.

---
+++ Seqdata.R
@@ -0,0 +1,21 @@
+#Create sequential data
+# Set number from 0 to 10 as y1;set number from 10 to 0 as y2
+y1 <- 0:10
+y1
+y2 <- 10:0
+y2
+#counts from 1 to 10
+y3 <- seq(10)
+y3
+?seq
+#counts down by 3
+y4 <- seq(30,0,by = -3)
+y4
+#Manually enter data for Concatenate
+y5 <- c(5,4,1,6,7,2,2,3,2,8)
+y5
+?c
+y6 <- scan()
+y6
+# list all of objects
+ls()
